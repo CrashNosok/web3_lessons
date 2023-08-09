3552b3c349a5fef2f327e4093333004ebce3f7f8
allwishes <141485713+allwishes@users.noreply.github.com>
2023-08-09 23:32:07 +0200

    Update main.py
    remove comments and add second print

--- web3_lesson_2/main.py
+++ web3_lesson_2/main.py
@@ -11,13 +11,7 @@ client = Client(private_key=private_key, network=Avalanche)
 client.w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 block = client.w3.eth.get_block('latest')
-# print(client.w3.eth.max_priority_fee)
-
-# woofi = WooFi(client=client)
-# amount = TokenAmount(amount=0.001)
-# tx = woofi.swap_eth_to_usdc(amount=amount)
-# res = woofi.client.verif_tx(tx_hash=tx)
-# print(res)
 
 res = Client.get_max_priority_fee_per_gas(w3=client.w3, block=block)
+print(res)
 print(res)
