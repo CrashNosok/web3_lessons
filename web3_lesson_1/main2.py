d3eb21681be0c3e54deef869ba4d01ec59be2ab2
crashnosok <riocrash@mail.ru>
2023-08-10 16:19:09 +0300

    remove comments from main2.py

--- web3_lesson_1/main2.py
+++ web3_lesson_1/main2.py
@@ -8,8 +8,6 @@ from data.config import private_key, arb_rpc
 client = Client(private_key=private_key, rpc=arb_rpc)
 
 woofi = WooFi(client=client)
-# amount = TokenAmount(amount=0.5, decimals=6)
-# tx = woofi.swap_eth_to_usdc(amount=amount)
 
 tx = woofi.swap_usdc_to_eth()
 
